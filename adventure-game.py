e3d3748da454b7e2e5f4bc4074e11b245952396f
Carmen Chen <carmen39@gmail.com>
2019-09-24 13:15:24 +0800

    feat: Adding the first_floor funtion
    Adding the first layer of interactive question with player.

--- adventure-game.py
+++ adventure-game.py
@@ -17,3 +17,30 @@ def intro():
     printpause("You enter the creepy castle.\n"
                "Right in front of you is a stairway that heads to 2nd floor"
                " and on your right hand cornor, you see a dark study room.\n")
+
+
+def first_floor(items):
+    first = input("Enter 1 to go on the stairway.\n"
+                  "Enter 2 to go to the study room.\n"
+                  "What would you like to do?\n"
+                  "(Please enter 1 or 2.)\n")
+    if first == '2':
+        if 'key' in items:
+            printpause("You've been here before."
+                       " There is nothing usable here.")
+            printpause("You are back to the entrance on the castle.\n")
+            first_floor(items)
+        else:
+            printpause("You are in the study room"
+                       " and you see something shining on the old bookshelf."
+                       " It's a key.\n")
+            printpause("You put the key in your pocket"
+                       " and head back to the castle entrance.\n")
+            printpause("You are back to the castle entrance.\n")
+            items.append("key")
+            first_floor(items)
+    elif first == '1':
+        second_floor(items)
+    else:
+        printpause("(Please enter 1 or 2.)\n")
+    first_floor(items)
